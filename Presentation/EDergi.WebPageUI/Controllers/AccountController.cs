a0bdb87854016d0613c9c438debd3d8e6db82742
Zehra Koç <130230313+zkoc1@users.noreply.github.com>
2025-08-18 00:25:37 +0300

    login

---
+++ Presentation/EDergi.WebPageUI/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using EDergi.Domain.Entitites;
+using WebPageUI.Areas.Admin.Models; // LoginViewModel'i buradan kullanıyoruz
+using System.Threading.Tasks;
+
+namespace WebPageUI.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly UserManager<AppUser> _userManager;
+
+		public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
+
+		[HttpGet]
+		[AllowAnonymous]
+		public IActionResult Login(string returnUrl = null)
+		{
+			ViewData["ReturnUrl"] = returnUrl;
+			return View();
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+		{
+			if (!ModelState.IsValid)
+				return View(model);
+
+			var user = await _userManager.FindByEmailAsync(model.Email);
+			if (user == null)
+			{
+				ModelState.AddModelError("", "E-posta veya şifre hatalı.");
+				return View(model);
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+			if (result.Succeeded)
+			{
+				if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+					return Redirect(returnUrl);
+
+				return RedirectToAction("Index", "Home"); // user giriş yaptıktan sonra yönlendirilecek sayfa
+			}
+
+			ModelState.AddModelError("", "E-posta veya şifre hatalı.");
+			return View(model);
+		}
+
+		
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Profile()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null) return RedirectToAction("Login");
+
+			return View(user); // Profile.cshtml açılır
+		}
+
+	}
+}
